579702745ccc760d50d4f34d262c52aa125a0f13
Stepan <stepanmaslov@gmail.com>
2023-04-20 14:03:39 +0300

    end task in Les2HW

--- src/main/java/Lesson2/task.java
+++ src/main/java/Lesson2/task.java
@@ -1,9 +1,65 @@
 package Lesson2;
 
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class task {
     public static void main(String[] args) {
-        System.out.println(zipString("aaabbdfffe"));
+        //System.out.println(zipString("aaabbdfffe"));
+        //HW2-----------------------------------
+        //task1();
+        task2();
     }
+
+    private static void task2() {
+        String catalog = "C:\\Users\\user\\Desktop\\Java\\.idea";
+        catalog = ".";
+        File dir = new File(catalog);
+        File[] test = dir.listFiles();
+        for (File temp: test){
+            if (!temp.isDirectory()){
+                String[] nameFile = temp.getName().split("\\.");
+                System.out.println("Расширение файла: " + nameFile[1]);
+            }
+        }
+    }
+
+    private static void task1() {
+
+        String jsonInput = "{name:Ivanov, country:Russia, city:Moscow, age:null}";
+        String[] jsonArrInput = jsonInput.replaceAll("[\\{\\}\\ ]", "").split(",");
+        Map<String, String> json = new HashMap<>();
+
+        for (String str : jsonArrInput) {
+            String[] temp = str.split(":");
+            json.put(temp[0], temp[1]);
+        }
+
+        String caption = "SELECT * FROM USER ";
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for (String key : json.keySet()) {
+            String temp1 = json.get(key);
+            String temp2 = "null";
+            //в чем отличие (!"null".equals(json.get(key))) от (!Object.equals("null", json.get(key))
+            if (!"null".equals(json.get(key))) {
+                if (!first) {
+                    result.append(" and ");
+                }
+                result.append(key + "=" + json.get(key));
+                first = false;
+            }
+        }
+        if (!first) {
+            result.insert(0, "WHERE \n");
+        }
+        System.out.println(caption + result.toString());
+    }
+
 
     public static String zipString(String inputStr) {
         StringBuilder result = new StringBuilder();
@@ -29,5 +85,6 @@ public class task {
         }
         return result.toString();
     }
+
 }
 
